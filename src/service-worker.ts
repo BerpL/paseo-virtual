dcb5fc39cde73bc64205e45e502c0259d4866c5d
B3rpL <eder.cornejo@softit.pe>
2023-11-02 19:50:00 -0500

    update sw

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -71,6 +71,20 @@ registerRoute(
 
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.jpg'),
+  // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  new StaleWhileRevalidate({
+    cacheName: 'imagesjpg',
+    plugins: [
+      // Ensure that once this runtime cache reaches a maximum size the
+      // least-recently used images are removed.
+      new ExpirationPlugin({ maxEntries: 50 }),
+    ],
+  })
+);
+
+registerRoute(
+  // Add in any other file extensions or routing criteria as needed.
   ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.js'),
   // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
